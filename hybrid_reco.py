eb0b4de5769dc7ccfabf4c8be2ea3a3f77bfdf70
thejoshuarenzo <jrdorado@up.edu.ph>
2024-08-26 17:20:39 +0800

    win prob

--- hybrid_reco.py
+++ hybrid_reco.py
@@ -102,8 +102,10 @@ def check_recos(is_ban, enemy_bans, enemy_picks, user_picks, recos, current_draf
                     if len(high_consequents) == 3:
                         break  # Stop after finding the top 3
             if len(low_consequents) + len(high_consequents) > 0:
-                print(f"Probably counter to enemy picks:\n\t{low_consequents}")
-                print(f"Combo your hero with these heroes:\n\t{high_consequents}")
+                print(f"Probably counter to enemy picks:")
+                print_list(low_consequents)
+                print(f"Combo your hero with these heroes:")
+                print_list(high_consequents)
                 print(f"Recommended {is_ban}s based on pick and bans order:\n\t{[hero_id_name(i) for i in recos[:3]]}")
             else:
                 print(f"Recommended {is_ban}s based on pick and bans order:\n\t{[hero_id_name(i) for i in recos[:3]]}")
@@ -133,7 +135,6 @@ def get_ban_first():
             return ban_first
         else:
             print("Invalid choice. Please enter 'Y' for Yes or 'N' for No.", "\n")
-
 
 # generate match id
 def generate_match_id(recent_match_id):
@@ -181,6 +182,32 @@ def slice_utility(utility_matrix, phase):
     cols = [col for col in utility_matrix.columns if int(str(col).split('_')[-1]) < phase]
     return utility_matrix[cols]
     
+def get_win_rate(only_drafts, is_team1=True):
+    # Calculate dot products and norms for similarity scores
+    last_row = only_drafts.iloc[-1].to_numpy()
+    matrix = only_drafts.to_numpy()
+    
+    dot_products = np.dot(matrix, last_row)
+    norms = np.linalg.norm(matrix, axis=1) * np.linalg.norm(last_row)
+    similarities = np.divide(dot_products, norms, out=np.zeros_like(dot_products), where=norms != 0)
+    
+    # Get the similarity scores for all matches except the last one
+    similarity_scores = pd.Series(similarities[:10], index=only_drafts.index[:10])
+    
+    # Use vectorized operations to calculate win count
+    match_ids = similarity_scores.index
+    radiant_wins = matches.loc[match_ids, 'radiant_win'].values
+    teams_selected = matches.loc[match_ids, 'team'].values
+    
+    # Determine the win conditions based on the team
+    if is_team1:
+        win_conditions = (teams_selected == 0) & (radiant_wins == True) | (teams_selected == 1) & (radiant_wins == False)
+    else:
+        win_conditions = (teams_selected == 0) & (radiant_wins == False) | (teams_selected == 1) & (radiant_wins == True)
+    
+    win_count = np.sum(win_conditions)
+    
+    return win_count
 
 def start_draft(utility_matrix, ban_first):
 
@@ -256,7 +283,8 @@ def start_draft(utility_matrix, ban_first):
         print(f'TEAM 1 PICKS: {picks_team1}\n')
         print(f'TEAM 2 BANS: {bans_team2}')
         print(f'TEAM 2 PICKS: {picks_team2}\n\n')
-
+    
+    return picks_team1, picks_team2
 
 
 if __name__ == "__main__":
@@ -265,7 +293,12 @@ if __name__ == "__main__":
 
     # load heroes reference
     df_heroes = pd.read_csv('./data/Constants/Constants.Heroes.csv', usecols=['id', 'localized_name'])
-
+    
+    # load matches reference for winrate
+    matches = pd.read_csv('./data/whole_draft.csv', index_col=0, low_memory=False)
+    
+    only_drafts = matches.drop(columns=['radiant_win', 'team'])
+    
     # import assoc rules
     with open('./data/rules.pkl', 'rb') as file:
         rules = pickle.load(file)
@@ -283,8 +316,38 @@ if __name__ == "__main__":
 
     # insert current match to utility matrix
     match_id = generate_match_id(draft_db)
+    draft_id = generate_match_id(only_drafts)
 
     # utility matrix for recommender
     utility_matrix = insert_match(filtered_db, match_id)
+    only_drafts = insert_match(only_drafts, draft_id)
+    
+    picks_1, picks_2 = start_draft(utility_matrix, ban_first)
+    
+    user_team, enemy_team = (picks_1, picks_2) if ban_first == 'Y' else (picks_2, picks_1)
+    
+    
+    # user_team = ['Shadow Fiend', 'Muerta']
+    # enemy_team = ['Bristleback', 'Slark']
+    
+    # Extract column indices based on hero IDs
+    user_ids = [hero_id_name(hero, 'id') for hero in user_team]
+    enemy_ids = [hero_id_name(hero, 'id') for hero in enemy_team]
 
-    start_draft(utility_matrix, ban_first)
+  
+    for id in user_ids:
+        only_drafts.loc[draft_id, id] = 1
+    for id in enemy_ids:
+        only_drafts.loc[draft_id, id] = 2
+
+    # Call the function to get win rate
+    sim1 = get_win_rate(only_drafts, True)
+    
+    for id in user_ids:
+        only_drafts.loc[draft_id, id] = 2
+    for id in enemy_ids:
+        only_drafts.loc[draft_id, id] = 1
+
+    sim2 = get_win_rate(only_drafts, False)
+    
+    print(f'Winning Probability: {(sim1+sim2)/20}')
